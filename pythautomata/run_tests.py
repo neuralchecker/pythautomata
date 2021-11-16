e6790bad7e27f66d860065862d837a3035a3d1ba
Federico Pan <federicopan86@gmail.com>
2021-11-15 21:32:28 -0300

    Added function to generate a PDFA from a DFA with random probability distributions

--- pythautomata/run_tests.py
+++ pythautomata/run_tests.py
@@ -1,17 +1,17 @@
 from unittest import TestLoader, TestSuite, TextTestRunner
 
-
 from pythautomata.tests.test_minimizer import TestMinimizer
 from pythautomata.tests.test_automata_convertor import TestAutomataConvertor
+from pythautomata.tests.test_pdfa_from_dfa import TestPDFAGenerator
 from pythautomata.tests.test_simple_DFA_generator import TestSimpleDFAGenerator
 from pythautomata.tests.test_automata_comparison import TestAutomataComparison
 from pythautomata.tests.test_sequence import TestSequence
 from pythautomata.tests.test_dfa_operations import TestDFAOperations
 from pythautomata.tests.test_automata_definitions import TestAutomataDefinitions
 
+
 def run():
-    test_classes_to_run = [TestMinimizer, TestAutomataConvertor, TestSimpleDFAGenerator, 
-        TestAutomataComparison, TestSequence, TestDFAOperations, TestAutomataDefinitions]
+    test_classes_to_run = [TestPDFAGenerator]
     loader = TestLoader()
     suites_list = []
     for test_class in test_classes_to_run:
