0394952fea5b6735ceb5dbc5488556a5b8c021c1
Martin Iturbide <martiniturbidenoria@gmail.com>
2023-02-14 18:10:21 -0300

    Added mealy machines and mealy states

---
+++ pythautomata/automata/mealy_machine.py
@@ -0,0 +1,113 @@
+import sys
+import uuid
+from pythautomata.base_types.alphabet import Alphabet
+from pythautomata.base_types.mealy_state import MealyState
+from pythautomata.base_types.sequence import Sequence
+from pythautomata.base_types.symbol import Symbol
+from pythautomata.exceptions.non_deterministic_states_exception import NonDeterministicStatesException
+from pythautomata.exceptions.unknown_symbols_exception import UnknownSymbolsException
+
+
+class MealyMachine:
+    """
+    Implementation of Mealy Machines.
+
+    Attributes
+    ----------
+    states: set[MealyState]
+        Set of states of the machine.
+    initial_state: MealyState
+        Initial state of the machine. Also included in "states".
+    input_alphabet: Alphabet
+        Set of Symbols that defines the Symbols that can be used as inputs.
+    output_alphabet: Alphabet
+        Set of Symbols that defines the Symbols that can be used as output.
+    name: str
+        Name of the machine.
+    exporting_strategies: list
+        List of exporting strategies that can be used to export the machine.
+    """
+
+    def __init__(self, input_alphabet: Alphabet, output_alphabet: Alphabet,
+                 initial_state: MealyState, states: set[MealyState], comparator,
+                 name: str = None, exporting_strategies: list = [],
+                 hole: MealyState = MealyState("\u22A5")):
+        self.states = states
+        for state in self.states:
+            self._verify_state(state, input_alphabet, output_alphabet)
+            state.add_hole_transition(hole)
+
+        self._name = ("Mealy Machine - " + str(uuid.uuid4().hex)
+                      if name is None else name)
+        self._input_alphabet = input_alphabet
+        self._output_alphabet = output_alphabet
+        self.initial_state = initial_state
+        self._set_hole(hole)
+        self._exporting_strategies = exporting_strategies
+        self._comparator = comparator
+        self._actual_state = initial_state
+
+    @property
+    def initial_states(self) -> frozenset:
+        return frozenset([self.initial_state])
+
+    @property
+    def hole(self):
+        return self._hole
+
+    def step(self, symbol):
+        output = self._actual_state.transitions[symbol]
+        self._actual_state = self._actual_state.next_state_for(symbol)
+
+        return output
+
+    def reset(self):
+        self._actual_state = self.initial_state
+
+    def last_symbol(self, sequence: Sequence) -> Symbol:
+        actual_state = self.initial_state
+        output = Symbol()
+        for symbol in sequence.value:
+            output = actual_state.outputs[symbol]
+            actual_state = actual_state.next_state_for(symbol)
+        return output
+
+    def transduce(self, sequence: Sequence) -> Sequence:
+        actual_state = self.initial_state
+        output = Sequence()
+        for symbol in sequence.value:
+            output.append(actual_state.outputs[symbol])
+            actual_state = actual_state.next_state_for(symbol)
+        return output
+
+    def export(self, path=None):
+        for strategy in self._exporting_strategies:
+            try:
+                strategy.export(self, path)
+            except:
+                print("Unexpected exception when exporting " +
+                      str(self._name) + ": " + str(sys.exc_info()[0]))
+
+    def _set_hole(self, hole: MealyState) -> None:
+        self._hole = hole
+        self.hole.add_hole_transition(hole)
+
+    def _verify_state(self, state: MealyState, input_alphabet: Alphabet,
+                      output_alphabet: Alphabet) -> None:
+        self.verify_transitions(state, input_alphabet)
+        self.verify_output(state, output_alphabet)
+        self.verify_state_is_deterministic(state)
+
+    def verify_transitions(self, state: MealyState, input_alphabet: Alphabet):
+        for symbol in state.transitions:
+            if symbol not in input_alphabet:
+                raise UnknownSymbolsException()
+
+    def verify_output(self, state: MealyState, output_alphabet: Alphabet):
+        for symbol in state.outputs:
+            if symbol not in output_alphabet:
+                raise UnknownSymbolsException()
+
+    def verify_state_is_deterministic(self, state: MealyState):
+        if not state.is_deterministic:
+            raise NonDeterministicStatesException()
---
+++ pythautomata/base_types/mealy_state.py
@@ -0,0 +1,63 @@
+from pythautomata.base_types.symbol import Symbol
+from pythautomata.exceptions.none_state_exception import NoneStateException
+
+
+class MealyState:
+    """Representation of Moore machine states.
+
+  Attributes
+  ----------
+  name: str
+      State name.
+  value: Symbol
+      Is an element of an alphabet associated with the state.
+  transitions: dict[Symbol, set['State']]
+      For any given symbol represents the next state (or set of states in the case of NFA).
+  hole:
+      Hole state, state containing all transitions directed to itself.
+      It is used as default when a symbol is not present as transition key.
+  """
+
+    def __init__(self, name: str):
+        self.name = name
+        self.transitions: dict[Symbol, 'MealyState'] = {}
+        self.outputs: dict[Symbol, Symbol] = {}
+        self._is_deterministic: bool = True
+
+    @property
+    def is_deterministic(self) -> bool:
+        return self._is_deterministic
+
+    def add_transition(self, symbol: Symbol, next_state: 'MealyState', output: Symbol) -> None:
+        if next_state is None:
+            raise NoneStateException()
+        if symbol not in self.transitions:
+            self.transitions[symbol] = next_state
+            self.outputs[symbol] = output
+        else:
+            self._is_deterministic = False
+
+    def next_states_for(self, symbol: Symbol) -> set['MealyState']:
+        if symbol not in self.transitions:
+            return set()
+        return {self.transitions[symbol]}
+
+    def next_state_for(self, symbol: Symbol) -> 'MealyState':
+        next_states = list(self.next_states_for(symbol))
+        assert (len(next_states) == 1)
+        return next_states[0]
+
+    def add_hole_transition(self, hole: 'MealyState') -> None:
+        self.hole = hole
+
+    def __eq__(self, other):
+        return isinstance(other, MealyState) and self.name == other.name
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __str__(self):
+        return repr(self)
+
+    def __repr__(self):
+        return str(self.name)
