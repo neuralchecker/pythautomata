287898c534317c7a00dd1146bca7e3e13fa423e2
juan pedro da silva <juanpedrodasilvabarloco@gmail.com>
2024-01-24 16:39:50 -0300

    Added State prefix random walk

---
+++ pythautomata/model_comparators/state_prefix_random_walk.py
@@ -0,0 +1,26 @@
+import random
+
+class StatePrefixRandomWalkComparisonStrategy:
+
+    def init(self, number_of_steps, reset_probability):
+        self.steps = number_of_steps
+        self.reset_prob = reset_probability
+
+    def are_equivalent(self, model1, model2):
+        return self.get_counterexample(model1, model2) is None
+
+    def get_counterexample(self, hypothesis, oracle):
+        for state in hypothesis.states:
+            prefix = self.get_access_string(hypothesis, state)
+
+            suffix = ""
+            for _ in range(self.steps):
+                suffix += random.choice(hypothesis.alphabet)
+
+                if hypothesis.process_query(prefix+suffix) != oracle.process_query(prefix+suffix):
+                    return prefix+suffix
+
+                if random.choices([True, False], [self.reset_prob, 1-self.reset_prob]):
+                    suffix = ""
+                
+        return None
