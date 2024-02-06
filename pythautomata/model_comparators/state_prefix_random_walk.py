fc184c9390593e855f9a8cff05bd3fcb57e37d4a
AlejoGarat <alejogaratd@gmail.com>
2024-02-06 16:42:46 -0300

    Fixed suffix bug

--- pythautomata/model_comparators/state_prefix_random_walk.py
+++ pythautomata/model_comparators/state_prefix_random_walk.py
@@ -1,9 +1,12 @@
 import random
+from random import randint
+
 from pythautomata.abstract.finite_automaton import FiniteAutomaton
+from pythautomata.base_types.sequence import Sequence
 
 class StatePrefixRandomWalkComparisonStrategy:
 
-    def init(self, number_of_steps, reset_probability):
+    def  __init__(self, number_of_steps, reset_probability):
         self.steps = number_of_steps
         self.reset_prob = reset_probability
 
@@ -11,17 +14,21 @@ class StatePrefixRandomWalkComparisonStrategy:
         return self.get_counterexample(model1, model2) is None
 
     def get_counterexample(self, hypothesis: FiniteAutomaton, oracle):
+        symbols = list(hypothesis.alphabet.symbols)
+        symbols.sort()
+        
         for state in hypothesis.states:
             prefix = hypothesis.get_access_string(state)
 
-            suffix = ""
+            suffix = Sequence()
             for _ in range(self.steps):
-                suffix += random.choice(hypothesis.alphabet)
+                pos = randint(0, len(symbols) - 1)
+                suffix = suffix.append(symbols[pos])
 
                 if hypothesis.process_query(prefix+suffix) != oracle.process_query(prefix+suffix):
                     return prefix+suffix
 
                 if random.choices([True, False], [self.reset_prob, 1-self.reset_prob]):
-                    suffix = ""
+                    suffix = Sequence()
                 
         return None
