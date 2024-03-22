665b979ba764594057c5b09d3c08d5efa2fd1da9
Franz28 <franzmayrojeda@gmail.com>
2024-03-22 10:58:38 -0300

    added fix

--- pythautomata/utilities/guiding_pdfa_sequence_generator.py
+++ pythautomata/utilities/guiding_pdfa_sequence_generator.py
@@ -4,10 +4,10 @@ from pythautomata.base_types.sequence import Sequence
 import random
 
 class GuidingPDFASequenceGenerator(SequenceGenerator):
-    def __init__(self, pdfa: PDFA, max_seq_lenght: int, random_seed: int = 21, random_stop_proba = 0.2):
+    def __init__(self, pdfa: PDFA, max_seq_lenght: int, random_seed: int = 21, retry = False):
         self.pdfa = pdfa
-        self._random_stop_proba = random_stop_proba
         self.max_seq_length = max_seq_lenght
+        self.retry = retry
         super().__init__(pdfa.alphabet, max_seq_lenght, random_seed)
 
     def generate_words(self, number_of_words: int):
@@ -17,9 +17,7 @@ class GuidingPDFASequenceGenerator(SequenceGenerator):
             result.append(word)
         return result
     
-    def _sort_valid_symbol(self, symbols, weights):
-        if random.random() < self._random_stop_proba:
-            return self.pdfa.terminal_symbol
+    def _sort_valid_symbol(self, symbols, weights):        
         selected_symbols = []
         for i,w in enumerate(weights):
             if w>0:
@@ -27,22 +25,24 @@ class GuidingPDFASequenceGenerator(SequenceGenerator):
         next_symbol = random.choice(selected_symbols)
         return next_symbol
     
-    def generate_single_word(self, length):
+    def generate_single_word_up_to_length(self, length):
         word = Sequence()
         first_state = list(filter(lambda x: x.initial_weight == 1, self.pdfa.weighted_states))[0]
         symbols, weights, next_states = first_state.get_all_symbol_weights()
         next_symbol = self._sort_valid_symbol(symbols, weights)
-        terminal_state = False
-        while next_symbol != self.pdfa.terminal_symbol and (length is not None or len(word) <= length) and not terminal_state:
+        while next_symbol != self.pdfa.terminal_symbol and len(word) <= length:
             word += next_symbol
             i = symbols.index(next_symbol)
             next_state = next_states[i]
             symbols, weights, next_states = next_state.get_all_symbol_weights()
             next_symbol = self._sort_valid_symbol(symbols, weights)
             if next_symbol == self.pdfa.terminal_symbol:
-                word += next_symbol
-                terminal_state = True
-            else:
-                length += 1
-        print(len(word))
+                word += next_symbol            
+        is_valid = next_symbol == self.pdfa.terminal_symbol
+        return word, is_valid
+
+    def generate_single_word(self, length):
+        word, is_valid =  self.generate_single_word_up_to_length(length)
+        while not is_valid and self.retry:
+            word, is_valid =  self.generate_single_word_up_to_length(length)
         return word
