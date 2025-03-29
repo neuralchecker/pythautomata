c01a1b067a3eec65c21912945acca8e6af97b101
Franz28 <franzmayrojeda@gmail.com>
2025-03-29 14:11:55 -0300

    added possibility of a PDFA with zero transitions to be absorbent when being generated

--- pythautomata/tests/test_pdfa_generator.py
+++ pythautomata/tests/test_pdfa_generator.py
@@ -2,7 +2,9 @@ from unittest import TestCase
 
 from pythautomata.automata_definitions.tomitas_grammars import TomitasGrammars
 from pythautomata.utilities import pdfa_generator
-
+from pythautomata.utilities import nicaud_dfa_generator
+from pythautomata.base_types.alphabet import Alphabet
+from pythautomata.base_types.symbol import SymbolStr
 
 class TestPDFAGenerator(TestCase):
 
@@ -68,4 +70,16 @@ class TestPDFAGenerator(TestCase):
         dfa = TomitasGrammars.get_automaton_5()
         pdfa = pdfa_generator.pdfa_from_dfa(
             dfa, distributions=1, max_shift=0.1, zero_probability=1)
+        self.assertEqual(pdfa, pdfa)
+    
+    def test_13(self):
+        symbols = []
+        for i in range(10):
+            symbols.append(SymbolStr(str(i)))
+        mediumAlphabet = Alphabet(frozenset(symbols))
+        generated_automata = nicaud_dfa_generator.generate_dfa(
+            mediumAlphabet, 200)
+        dfa = generated_automata
+        pdfa = pdfa_generator.pdfa_from_dfa(
+            dfa, distributions=100, max_shift=0.3, zero_probability=0.5, make_absorbent=True)
         self.assertEqual(pdfa, pdfa)
